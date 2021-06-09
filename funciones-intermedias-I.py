345fa61dafb94fa9882ac1ee2ad0dbacc81556b3
Jimmy Duarte <jimmy.duarte86@gmail.com>
2021-06-09 19:04:04 -0400

    Funciones Intermedias I v2

--- funciones-intermedias-I.py
+++ funciones-intermedias-I.py
@@ -1,8 +1,26 @@
 import random
-def randInt(min=0, max=100):
-    num = random.random()
+def randInt(min=0 , max=100):
+    num = random.randint(min,max)
     return num
-print(randInt()) 		    # debería imprimir un número aleatorio entre 0 a 100
-print(randInt(max=50)) 	    # debería imprimir un número aleatorio entre 0 a 50
-print(randInt(min=50)) 	    # debería imprimir un número aleatorio entre 50 a 100
-print(randInt(min=50, max=500))    # debería imprimir un número aleatorio entre 50 y 500
+#print(randInt())
+# debería imprimir un número aleatorio entre 0 a 100
+#print(randInt(max=50))
+# debería imprimir un número aleatorio entre 0 a 50
+#print(randInt(min=50))
+# debería imprimir un número aleatorio entre 50 a 100
+print(randInt(min=50, max=500))
+# debería imprimir un número aleatorio entre 50 y 500
+
+
+import random
+def randInt(min=0 , max=100):
+    num = random.random(min,max)
+    return num
+#print(randInt())
+# debería imprimir un número aleatorio entre 0 a 100
+#print(randInt(max=50))
+# debería imprimir un número aleatorio entre 0 a 50
+#print(randInt(min=50))
+# debería imprimir un número aleatorio entre 50 a 100
+print(randInt(min=50, max=500))
+# debería imprimir un número aleatorio entre 50 y 500
